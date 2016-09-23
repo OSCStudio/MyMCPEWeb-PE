32c108ec6d15fbb055d1f3a705db51171f8dc38d
nfls <1105052411@qq.com>
2016-09-24 01:03:27 +0800

    Add support for json files

--- src/MyMCPEWeb/MyMCPEWeb.php
+++ src/MyMCPEWeb/MyMCPEWeb.php
@@ -5,11 +5,13 @@ namespace MyMCPEWeb;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
 use pocketmine\scheduler\CallbackTask;
 use pocketmine\Level\level;
 
 
 class MyMCPEWeb extends PluginBase{
+
 	
 	public function onEnable(){
 		$this->getLogger()->info(TextFormat::WHITE . "MinecraftPEWeb Enabled!!!" );
@@ -17,41 +19,30 @@ class MyMCPEWeb extends PluginBase{
 			$this,
 			"GetOnlinePlayers"
 		] ), 100 );
-		//$this->getLogger()->info(TextFormat::WHITE .$this->GetBiomeData() );
 	}
 	
 	public function GetOnlinePlayers()
 	{
-		//status of server is on
-		//$json_output= "{'status':'on'";
 		$ess_inf=array();
 		$ess_inf["status"]="on";
-		//time of the world
-		//$json_output .= ",'time':";
 		foreach($this->getServer()->getLevels() as $level){
 			if($level->getName()=='world')
 				$ess_inf["time"]=$level->getTime();
 		}
-		//player details
-		//$json_output .= ",'players':";
-		//$usr_inf=array();
-		//$added = false;
 		$onlineCount = 0;
 		foreach($this->getServer()->getOnlinePlayers() as $player){
 			if($player->isOnline()){
-				$ess_inf[$player->getDisplayName()][pos_x]=$player->getFloorX();
-				$ess_inf[$player->getDisplayName()][pos_y]=$player->getFloorY();
-				$ess_inf[$player->getDisplayName()][pos_z]=$player->getFloorZ();
-				$ess_inf[$player->getDisplayName()][world]=$player->getLevel()->getName();
-				//$json_output .= "'" . $player->getDisplayName() . "':{'posx':".$player->getFloorX()."','posy':".$player->getFloorY()."','posz':".$player->getFloorZ()."','worldname':".$player->getLevel()->getName() ."'}";
+				$ess_inf[$player->getDisplayName()]['pos_x']=$player->getFloorX();
+				$ess_inf[$player->getDisplayName()]['pos_y']=$player->getFloorY();
+				$ess_inf[$player->getDisplayName()]['pos_z']=$player->getFloorZ();
+				$ess_inf[$player->getDisplayName()]['world']=$player->getLevel()->getName();
 				++$onlineCount;
 			}
 		}
 		$ess_inf["online_count"]=$onlineCount;
 		$this->getLogger()->info(TextFormat::WHITE . json_encode($ess_inf));
-		//$this->getLogger()->info(TextFormat::WHITE . json_encode($usr_inf));
+		file_put_contents($this->getDataFolder().'ser_inf.json', json_encode($ess_inf));
 	}
-	
 	/* This function will be not available until the website is all working properly.
 	public function GetBiomeData()
 	{
